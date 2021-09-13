776970d697a4b40b769dbfaa4a86d414f194005f
biaoHuynh <hnquocbao2>
2021-09-13 22:39:48 +0700

    start JPA

---
+++ src/main/java/com/javaspringjpa/huynhnguyenquocbao/HuynhNguyenQuocBaoApplication.java
@@ -0,0 +1,13 @@
+package com.javaspringjpa.huynhnguyenquocbao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuynhNguyenQuocBaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuynhNguyenQuocBaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaspringjpa/huynhnguyenquocbao/HuynhNguyenQuocBaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspringjpa.huynhnguyenquocbao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuynhNguyenQuocBaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
